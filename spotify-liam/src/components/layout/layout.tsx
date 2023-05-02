a247d3ff80ac35031f08eb926a9f7396d4fc9451
Lam Nguyen <languyen@axon.com>
2023-05-02 19:56:29 +0700

    Add shared footer and add styles

--- spotify-liam/src/components/layout/layout.tsx
+++ spotify-liam/src/components/layout/layout.tsx
@@ -1,6 +1,7 @@
 import {Outlet} from "react-router-dom";
 import React from "react";
 import {NavBar} from "../navBar";
+import {Footer} from "../footer/footer";
 
 
 export const Layout: React.FC = () => {
@@ -8,6 +9,7 @@ export const Layout: React.FC = () => {
         <>
             <NavBar />
             <Outlet />
+            <Footer />
         </>
 
     )
