8c9e36d5e64f0a965e2e7f79677516ac7a8860ec
Lam Nguyen <languyen@axon.com>
2023-05-03 10:16:20 +0700

    Add logo spotify on header

--- spotify-liam/src/components/navBar/navBar.tsx
+++ spotify-liam/src/components/navBar/navBar.tsx
@@ -1,30 +1,35 @@
 import {Link} from "react-router-dom";
 import React from "react";
 import './navBar.scss';
-
+import spotifyLogo from '../../statics/logo/spotify-logo-white.png'
 export const NavBar: React.FC = () => {
     return (
-        <nav className='navBar'>
-            <ul className='navBarList'>
-                <li>
-                    <Link to="/premium">Premium</Link>
-                </li>
-                <li>
-                    <Link to="/support">Support</Link>
-                </li>
-                <li>
-                    <Link to="/download">Download</Link>
-                </li>
-                <div className='separator'></div>
-                <li>
-                    <Link to="/signup">Signup</Link>
-                </li>
-                <li>
-                    <button className='loginButton'>
-                        <Link to="/login" className='loginText'>Login</Link>
-                    </button>
-                </li>
-            </ul>
-        </nav>
+        <header className='header'>
+            <div className='nav'>
+                <Link className='branding' to='/'><img src={spotifyLogo} alt='Spotify Log' width={132} height={40}/></Link>
+                <nav className='navBar'>
+                    <ul className='navBarList'>
+                        <li>
+                            <Link to="/premium">Premium</Link>
+                        </li>
+                        <li>
+                            <Link to="/support">Support</Link>
+                        </li>
+                        <li>
+                            <Link to="/download">Download</Link>
+                        </li>
+                        <div className='separator'></div>
+                        <li>
+                            <Link to="/signup">Signup</Link>
+                        </li>
+                        <li>
+                            <button className='loginButton'>
+                                <Link to="/login" className='loginText'>Login</Link>
+                            </button>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
     )
 }
